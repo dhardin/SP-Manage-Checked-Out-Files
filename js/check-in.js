430875386ababb1333e271b79b8459f9abbde3d1
Dustin Hardin <dhardin07@gmail.com>
2015-05-28 12:03:55 -0400

    parse url further

--- js/check-in.js
+++ js/check-in.js
@@ -107,6 +107,7 @@ var check_in = (function() {
         state_map.url = parseTo(state_map.url, '/Forms');
 
         listname = state_map.url.substring(state_map.url.lastIndexOf('/') + 1);
+        state_map.url = parseTo(state_map.url, '/' + listname);
 
         getListGUIDfromURL(state_map.url, listname, function(results){
             var guid = results.ID;
